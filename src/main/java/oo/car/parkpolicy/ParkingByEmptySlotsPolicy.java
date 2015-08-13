2cd802a07eb2612cec3ecb2f3b54e6ddc28b5a1d
zhuzhenguang <zhuzhenguang2008@gmail.com>
2015-08-13 22:32:28 +0800

    super boy refactor

--- src/main/java/oo/car/parkpolicy/ParkingByEmptySlotsPolicy.java
+++ src/main/java/oo/car/parkpolicy/ParkingByEmptySlotsPolicy.java
@@ -2,16 +2,21 @@ package oo.car.parkpolicy;
 
 import oo.car.ParkingLot;
 
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Optional;
 
 /**
  * by empty slots
  * <p/>
  * Created by twer on 15/8/8.
  */
-public class ParkingByEmptySlotsPolicy extends ParkingByMaxValuePolicy {
+public class ParkingByEmptySlotsPolicy implements ParkPolicy {
+    public static final Comparator<ParkingLot> PARKING_LOT_COMPARATOR = (o1, o2) ->
+            Integer.valueOf(o1.emptySlot()).compareTo(o2.emptySlot());
+
     @Override
-    Comparator<ParkingLot> parkingLotComparator() {
-        return (o1, o2) -> Integer.valueOf(o1.emptySlot()).compareTo(o2.emptySlot());
+    public Optional<ParkingLot> choose(ParkingLot[] parkingLots) {
+        return Arrays.asList(parkingLots).stream().max(PARKING_LOT_COMPARATOR);
     }
 }
--- src/main/java/oo/car/parkpolicy/ParkingByVacancyRatePolicy.java
+++ src/main/java/oo/car/parkpolicy/ParkingByVacancyRatePolicy.java
@@ -2,16 +2,21 @@ package oo.car.parkpolicy;
 
 import oo.car.ParkingLot;
 
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Optional;
 
 /**
  * VacancyRate
  * <p/>
  * Created by twer on 15/8/11.
  */
-public class ParkingByVacancyRatePolicy extends ParkingByMaxValuePolicy {
+public class ParkingByVacancyRatePolicy implements ParkPolicy {
+    public static final Comparator<ParkingLot> PARKING_LOT_COMPARATOR = (o1, o2) ->
+            Double.valueOf(o1.vacancyRate()).compareTo(o2.vacancyRate());
+
     @Override
-    Comparator<ParkingLot> parkingLotComparator() {
-        return (o1, o2) -> Double.valueOf(o1.vacancyRate()).compareTo(o2.vacancyRate());
+    public Optional<ParkingLot> choose(ParkingLot[] parkingLots) {
+        return Arrays.asList(parkingLots).stream().max(PARKING_LOT_COMPARATOR);
     }
 }
